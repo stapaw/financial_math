07f7aae81eb19ae4b5b76492b4412264a06c6039
Stanislaw Pawlak <stanislawpawlak3@gmail.com>
2020-03-26 17:29:22 +0100

    add: lab2 ex. 3 and 5

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -30,15 +30,24 @@ for x in range(0, 10000):
 
 data = np.arange(0, 25, 0.1)
 plt.hist(results, bins=data)
+
+
 # plt.show()
 
 # ex. 2
+def generate_wiener_trajectory1(T):
+    t, w = [i for i in range(0, T + 1)], [0]
+    for i in t[:-1]:
+        w.append(w[i] + math.sqrt(t[i + 1] - t[i]) * np.random.normal())
+    return w
+
+
 t = [i for i in range(0, 26)]
-w = [0 for i in range(0, 26)]
-for i in t[:-1]:
-    w[i + 1] = w[i] + math.sqrt(t[i + 1] - t[i]) * np.random.normal()
+w = generate_wiener_trajectory1(25)
 
 plt.step(t, w)
+
+
 # plt.show()
 
 
@@ -48,8 +57,9 @@ def create_sigma(T):
     sigma = [[0.0] * T for i in range(0, T)]
     for i in range(0, T):
         for j in range(0, T):
-            sigma[i][j] = min(i+1, j+1)
+            sigma[i][j] = min(i + 1, j + 1)
     return sigma
+
 
 def cholesky(S):
     n = len(S)
@@ -63,9 +73,41 @@ def cholesky(S):
                 L[i][k] = (1.0 / L[k][k] * (S[i][k] - tmp_sum))
     return L
 
-T=10
-sigma = create_sigma(T-1)
+
+T = 10
+sigma = create_sigma(T - 1)
 L = np.array(cholesky(sigma))
-X = np.array([np.random.normal() for i in range(0, T-1)])
-w = [0] + list(L@X)
+X = np.array([np.random.normal() for i in range(0, T - 1)])
+w = [0] + list(L @ X)
 print(w)
+
+# ex 3
+
+w1 = generate_wiener_trajectory1(50)
+w2 = generate_wiener_trajectory1(50)
+
+# plt.show()
+plt.step(w1, w2)
+plt.show()
+
+
+# ex 5
+def f(t):
+    return math.sqrt(2 * t * math.log(math.log(t)))
+
+
+def generate_wiener_trajectories(n, T, data):
+    for i in range(0, n):
+        w = generate_wiener_trajectory1(T)
+        plot_wiener_trajectory(w, data)
+
+
+def plot_wiener_trajectory(w, data):
+    plt.step(data, [w[x] for x in data])
+
+
+data = np.arange(10, 100000, 1000)
+plt.plot(data, [f(t) for t in data])
+plt.plot(data, [-f(t) for t in data])
+generate_wiener_trajectories(30, 100000, data)
+plt.show()
